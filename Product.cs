b1cf2f6e437e4a5f69690736ca6fa7acdf26b1fa
Michelle Stevens <stevens.michellec@gmail.com>
2021-06-06 18:07:39 -0700

    Api jason

---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mock_BestBuy_API
+{
+    public class Product
+    {
+      
+        public int ProductID { get; set; }
+
+        [BindProperty]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public double Price { get; set; }
+
+        [BindProperty]
+        public int CategoryID { get; set; }
+
+        public bool OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+
+
+    }
+}
+
